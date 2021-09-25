a6debae14712c75095ce2ea52ca8b976019e206a
WMSNTANA <webmunisantaana@gmail.com>
2021-09-25 14:59:01 +0000

    Ajustes Theme11

--- ckanext/example_theme/plugin.py
+++ ckanext/example_theme/plugin.py
@@ -1,16 +1,23 @@
+# encoding: utf-8
+
+'''plugin.py
+
+'''
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from ckan.common import config
+
 
 class ExampleThemePlugin(plugins.SingletonPlugin):
+    '''An example theme plugin.
 
-# Declare that this class implements IConfigurer.
-  plugins.implements(plugins.IConfigurer)
+    '''
+    # Declare that this class implements IConfigurer.
+    plugins.implements(plugins.IConfigurer)
 
     def update_config(self, config):
 
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
+        # 'templates' is the path to the templates dir, relative to this
+        # plugin.py file.
         toolkit.add_template_directory(config, 'templates')
-        toolkit.add_public_directory(config, 'public')
-        toolkit.add_resource('example_theme')
