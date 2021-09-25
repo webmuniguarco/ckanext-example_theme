7740cf7ed37c5478c4554526ff5546b8643b25a4
WMSNTANA <webmunisantaana@gmail.com>
2021-09-25 14:24:01 +0000

    Ajustes Theme8

--- ckanext/example_theme/plugin.py
+++ ckanext/example_theme/plugin.py
@@ -11,6 +11,10 @@ class ExampleThemePlugin(plugins.SingletonPlugin):
 
         def update_config(self, config):
 
-                # Add this plugin's templates dir to CKAN's extra_template_paths, so
-                # that CKAN will use this plugin's custom templates.
-                toolkit.add_template_directory(config, 'templates')
+    # Add this plugin's templates dir to CKAN's extra_template_paths, so
+    # that CKAN will use this plugin's custom templates.
+    toolkit.add_template_directory(config, 'templates')
+
+    # Add this plugin's public dir to CKAN's extra_public_paths, so
+    # that CKAN will use this plugin's custom static files.
+    toolkit.add_public_directory(config, 'public')
